0ff0d483319c53146f286ecf6045632a1fe3806e
shogo-shimizu <shimizu.martin@gmail.com>
2023-04-22 23:36:23 +0900

    ver1.0

--- single.php
+++ single.php
@@ -9,17 +9,24 @@
     <header>
         <div class="headerBackImage"></div>
         <?php include(get_template_directory() . '/inc/navbar.php'); ?>
+        <h1 class="borderAnime"><?php the_title(); ?></h1>
     </header>
 
     <main>
         <div class="content">
-            <h1 class="borderAnime"><?php the_title(); ?></h1>
+            <p><?php the_time(get_option('date_format')); ?></p>
+            <span class="categoryLinks">
+                <?php the_category('&nbsp'); ?>
+            </span>
+            <span class="tagLinks">
+                <?php the_tags('', '', ''); ?>
+            </span>
             <div class="singleThumbnail">
-            <?php 
-                if(has_post_thumbnail()){
+                <?php
+                if (has_post_thumbnail()) {
                     the_post_thumbnail();
                 }
-             ?>
+                ?>
             </div>
             <?php the_content(); ?>
         </div>
