710f69c228e04de4b6b093b9cc806f0e0c577e91
shogo-shimizu <shimizu.martin@gmail.com>
2023-04-25 13:15:06 +0900

    home design fix for pc

--- footer.php
+++ footer.php
@@ -9,11 +9,23 @@
     </div>
     <div class="footerInfo">
         <h2>Blog Infomation</h2>
-        <p>お問合せ</p>
+        <!-- <p>お問合せ</p>
         <a href="mailto:<?php bloginfo('admin_email') ?>">
             <p><?php bloginfo('admin_email') ?></p>
-        </a>
-        <p></p>
+        </a> -->
+        <p>Blogger</p>
+        <?php $users = get_users(array(
+            'orderby' => 'ID',
+            'order' => 'ASC'
+        )); ?>
+        <ul class="users_list">
+            <?php foreach ($users as $user) {
+                $uid = $user->ID; ?>
+                <li>
+                    <a href="<?php echo get_bloginfo("url") . '/?author=' . $uid ?>"><?php echo $user->display_name; ?></a>
+                </li>
+            <?php } ?>
+        </ul>
     </div>
     <!-- <div class="footerContact">
         <h2>Contact</h2>
